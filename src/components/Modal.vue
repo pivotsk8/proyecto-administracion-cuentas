a1e7741a623e1ed4230c52598a72e0dcba06f669
piiivot <the_pivotsk8@hotmail.com>
2023-06-21 15:34:02 -0500

    feat: Se le dieron estilos al formulario del modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -14,7 +14,7 @@ const emit = defineEmits(['ocultar-modal']);
     </div>
 
     <div class="contenedor">
-      <form action="">
+      <form class="nuevo-gasto">
         <legend>Añadir Gastos</legend>
         <div class="campo">
           <label for="nombre">Nombre Gasto:</label>
@@ -32,6 +32,7 @@ const emit = defineEmits(['ocultar-modal']);
         <div class="campo">
           <label for="categoria">Categoria:</label>
           <select id="categoria">
+            <option value="">-- Selecciona --</option>
             <option value="ahorro">Ahorro</option>
             <option value="comida">Comida</option>
             <option value="casa">Casa</option>
@@ -41,7 +42,7 @@ const emit = defineEmits(['ocultar-modal']);
             <option value="suscripciones">Suscripciones</option>
           </select>
         </div>
-        <input type="submit" value="Añadir gasto"/>
+        <input type="submit" value="Añadir gasto" />
       </form>
     </div>
   </div>
@@ -63,6 +64,42 @@ const emit = defineEmits(['ocultar-modal']);
 }
 .cerrar-modal img {
   width: 3rem;
+  cursor: pointer;
+}
+.nuevo-gasto {
+  margin: 10rem auto 0 auto;
+  display: grid;
+  gap: 2rem;
+}
+.nuevo-gasto legend {
+  text-align: center;
+  color: var(--blanco);
+  font-size: 3rem;
+  font-weight: 700;
+}
+.campo {
+  display: grid;
+  gap: 2rem;
+}
+
+.nuevo-gasto input,
+.nuevo-gasto select {
+  background-color: var(--gris-claro);
+  border-radius: 1rem;
+  padding: 1rem;
+  border: none;
+  font-size: 2.2rem;
+}
+
+.nuevo-gasto label {
+  color: var(--blanco);
+  font-size: 3rem;
+}
+
+.nuevo-gasto input[type='submit'] {
+  background-color: var(--azul);
+  color: var(--blanco);
+  font-weight: 700;
   cursor: pointer;
 }
 </style>
